286c70ffcfd390056e1e64be71188759b9540e0e
sunil9119 <[sunil9119@gmail.com]>
2020-01-23 05:26:04 -0600

    Initial Commit

---
+++ Schedular.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Schedular.API.Models;
+
+namespace Schedular.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ Schedular.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace Schedular.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
